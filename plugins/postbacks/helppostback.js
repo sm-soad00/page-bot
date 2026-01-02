4884a1e97722a7cdcdd1eff5023dd2472c53b930
Mohammad Nayan <mohammadnayan1669@gmail.com>
2026-01-02 18:02:25 +0600

    Delete plugins/postbacks/helppostback.js

--- plugins/postbacks/helppostback.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  config: {
-    payload: "help"
-  },
-  start: async ({ senderId, nayan, event }) => {
-    console.log(event)
-    return nayan.sendMessage(senderId, { text: "This is help info via postback button." });
-  }
-};
